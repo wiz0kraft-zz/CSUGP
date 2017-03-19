1f07a6736cb89ead7100d71154db3c3b98f04151
wiz0kraft <abhinavprakash096shift@gmail.com>
2017-03-19 05:30:38 +0530

    Add files via upload

---
+++ ugp-facedet.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import numpy as np
+import cv2
+import cv2.cv as cv
+from video import create_capture
+from common import clock, draw_str
+
+help_message = '''
+USAGE: facedetect.py [--cascade <cascade_fn>] [--nested-cascade <cascade_fn>] [<video_source>]
+'''
+
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,2:] += rects[:,:2]
+    return rects
+
+def draw_rects(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
+if __name__ == '__main__':
+    import sys, getopt
+    print help_message
+
+    args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade=', 'nested-cascade='])
+    try: video_src = video_src[0]
+    except: video_src = 0
+    args = dict(args)
+    cascade_fn = args.get('--cascade', "../../data/haarcascades/haarcascade_frontalface_alt.xml")
+    nested_fn  = args.get('--nested-cascade', "../../data/haarcascades/haarcascade_eye.xml")
+
+    cascade = cv2.CascadeClassifier(cascade_fn)
+    nested = cv2.CascadeClassifier(nested_fn)
+
+    cam = cv2.VideoCapture('/home/wizkraft/Videos/NarcosS01E01.mkv')
+    i=0;
+    j=1;
+    while True:
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.equalizeHist(gray)
+
+        t = clock()
+        if i%25 == 0:
+            rects = detect(gray, cascade)
+            #print(rects)
+            #crop_img = img(rects)
+            #crop_img.save('_0.png')
+            if not type(rects) is list : 
+                crop_img = img[rects[0,1]:rects[0,3], rects[0,0]:rects[0,2]]
+                cv2.imwrite("/home/wizkraft/Desktop/Faces/NarcosS01E01/"+str(j)+".png",crop_img)
+                j = j+1
+        vis = img.copy()
+        draw_rects(vis, rects, (0, 255, 0))
+
+        dt = clock() - t
+
+        draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+        cv2.imshow('facedetect', vis)
+        i = i+1
+
+        if 0xFF & cv2.waitKey(5) == 27:
+            break
+    cv2.destroyAllWindows()
